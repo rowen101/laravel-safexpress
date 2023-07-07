08f26e5680f796db8a9b90a22c4b400a44b6b524
Rowen <rgrowengonzales66@gmail.com>
2023-07-07 15:31:42 +0800

    update

---
+++ resources/js/app/pages/App.vue
@@ -0,0 +1,69 @@
+<template>
+ <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                Laravel
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+
+                                <li class="nav-item">
+                                     <router-link class="nav-link" to="/">Home</router-link>
+                                </li>
+
+                                <li class="nav-item">
+                                     <router-link  class="nav-link" to="/about">About</router-link>
+                                </li>
+
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                  System
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#"
+                                      >
+                                     Logout
+                                    </a>
+
+
+                                </div>
+                            </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+         <main class="py-4">
+ <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+
+                       <router-view/>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+         </main>
+
+</template>
+<script>
+export default {
+    name: "App",
+};
+</script>
