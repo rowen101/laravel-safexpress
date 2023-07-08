13d884c3818274e142bef3388768f5b80941f0b4
Rowen <rgrowengonzales66@gmail.com>
2023-07-08 13:14:44 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,18 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
-  $visited = DB::select('select * from places where visited = ?', [1]); 
-  $togo = DB::select('select * from places where visited = ?', [0]);
-
-  return view('travel_list', ['visited' => $visited, 'togo' => $togo ] );
+    return view('welcome');
 });
