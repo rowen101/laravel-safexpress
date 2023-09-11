6b186a542c24287eecaa6dc016f8a5c5945d2da6
Rowen <rgrowengonzales66@gmail.com>
2023-09-11 20:42:03 +0800

    update

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -154,7 +154,7 @@ class PagesController extends Controller
     }
     public function transport()
     {
-        $title = "Transport Serveces";
+        $title = "Transport Services";
         $menuItem = Menu::where('is_active', 1)
         ->where('app_id', 2)
         ->where('parent_id', 0)
