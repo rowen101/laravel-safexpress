4f534c55f280f9cea10cc34439273787eb4df628
Rowen <rgrowengonzales66@gmail.com>
2023-10-23 13:44:16 +0800

    update

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -542,7 +542,7 @@
             <div class="container">
                 <div class="row gy-4">
                     <div class="col-lg-5 col-md-12 footer-info">
-                        <a href="index.html" class="logo d-flex align-items-center">
+                        <a href="#" class="logo d-flex align-items-center">
                             <span>Safexpress</span>
                         </a>
                         <p>{{ $setting->site_address }}</p>
